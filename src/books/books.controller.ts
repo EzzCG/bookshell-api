3d05b911a969dc4e61c34bea3f977ece4e60d3ec
naez@alumni.uv.es <72402860+officialEN10@users.noreply.github.com>
2026-04-06 13:23:39 +0200

    fix(config): correct setup/path config

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -7,12 +7,12 @@ import {
   Param,
   Body,
   Query,
-  UseGuards,
+  // UseGuards,
 } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+// import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 // @UseGuards(JwtAuthGuard)
 @Controller('books')
